d4f0994942a55e5306ced67b9a03aa3f1eda6b25
Manuel <manuelhany@gmail.com>
2024-02-12 16:14:58 +0200

    Status Codes

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
@@ -23,7 +24,11 @@ def movie_list(request):
 def movie_details(request, pk):
     
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
@@ -34,10 +39,10 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
