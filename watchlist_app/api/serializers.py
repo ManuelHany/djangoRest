56c6434e4f8f34f34b3c21afa6772965c990c532
Manuel <manuelhany@gmail.com>
2024-02-12 15:56:01 +0200

    Serializers - POST, PUT, Delete Requests

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+    
+    def create(self, valicated_data):
+        return Movie.objects.create(**valicated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        put or patch
+        Args:
+            instance (Movie): old update
+            validated_data (Movie): new update
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
