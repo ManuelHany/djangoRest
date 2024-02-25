bc0dc386708ae3c78cc5d59b45653525f7c29f19
Manuel <manuelhany@gmail.com>
2024-02-25 16:32:08 +0200

    10- Django Relationships

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -13,8 +13,9 @@ class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
     active = models.BooleanField(default=True) # if the movie is released or launched
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist", default=None)
     created = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.title
-    
+     
